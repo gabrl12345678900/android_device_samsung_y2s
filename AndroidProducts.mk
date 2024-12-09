e87ac00791a14919c814b79ece3633be31255356
Ronnz98 <56398486+Ronnz98@users.noreply.github.com>
2024-12-09 11:31:30 +0100

    Update AndroidProducts.mk
    Support lineage again

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_y2s.mk
+    $(LOCAL_DIR)/lineage_y2s.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_y2s-user \
-    derp_y2s-userdebug \
-    derp_y2s-eng
+    lineage_y2s-user \
+    lineage_y2s-userdebug \
+    lineage_y2s-eng
